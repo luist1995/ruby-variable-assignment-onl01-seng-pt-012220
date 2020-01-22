79edb6433f80e058e2b5df13afada3ca06c83197
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-01-22 04:40:40 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,3 +1,6 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
  greeting = "hello World"
+ name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow!  ' + name + ' is a really long name!'
